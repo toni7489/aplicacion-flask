17337d730bb5451c34cf03f56db7db9fa7e425ee
Toni <50716150+seokom@users.noreply.github.com>
2024-12-07 18:10:04 +0100

    app12

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import pyshorteners
 import os
 import random
 import string
+import qrcode
+from io import BytesIO
+import base64
+import qrcode
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -48,5 +54,53 @@ def password():
 def colorpicker():
     return render_template('colorpicker.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
+#Generador codigo QR    
+@app.route('/qr', methods=['GET', 'POST'])
+def index():
+    qr_code = None
+    if request.method == 'POST':
+        url = request.form['url']
+        if url:  # Verificar si la URL está presente
+            try:
+                # Generar el código QR
+                img = qrcode.make(url)
+
+                # Guardar la imagen en un buffer en memoria
+                img_io = io.BytesIO()
+                img.save(img_io, 'PNG')
+                img_io.seek(0)
+
+                # Convertir la imagen a base64 para incrustarla en el HTML
+                qr_code = base64.b64encode(img_io.getvalue()).decode('utf-8')
+            except Exception as e:
+                # Si ocurre un error al generar el código QR
+                return f"Error al generar el código QR: {e}"
+        
+        # Asegurarse de devolver la plantilla
+        return render_template('qr.html', qr_code=qr_code)
+
+    # Si el método es GET, solo renderizamos el formulario vacío
+    return render_template('qr.html', qr_code=None)
+
+
+@app.route('/download_qr')
+def download_qr():
+    url = request.args.get('url')
+
+    if not url:
+        return "URL no proporcionada", 400
+
+    # Generar el código QR
+    img = qrcode.make(url)
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    # Enviar la imagen como archivo descargable
+    return send_file(img_io, as_attachment=True, download_name='codigo_qr.png', mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

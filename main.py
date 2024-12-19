eedef693f57f25592abe87abd643ed5e1e00f36a
Toni <50716150+seokom@users.noreply.github.com>
2024-12-19 22:06:30 +0100

    appfotos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file,url_for
 import pyshorteners
 import os
 import random
@@ -158,7 +158,66 @@ def vulnerabilidades():
 
 
 
+@app.route("/fotos")
+def gallery():
+    # Lista de imágenes en la galería
+    images = [
+        "image1.jpg",
+        "image2.jpg",
+        "image3.jpg",
+        "image4.jpg",
+        "image5.jpg",
+        "image6.jpg",
+        "image7.jpg",
+        "image8.jpg",
+        "image9.jpg",
+        "image10.jpg",
+        "image11.jpg",
+        "image12.jpg",
+        "image13.jpg",
+        "image14.jpg",
+        "image15.jpg",
+        "image16.jpg",
+        "image17.jpg",
+        "image18.jpg",
+        "image19.jpg",
+        "image20.jpg",
+        "image21.jpg",
+        "image22.jpg",
+        "image23.jpg",
+        "image24.jpg",
+        "image25.jpg",
+        "image26.jpg",
+        "image27.jpg",
+        "image28.jpg",
+        "image29.jpg",
+        "image30.jpg",
+        "image31.jpg",
+        "image32.jpg",
+        "image33.jpg",
+        "image34.jpg",
+        "image35.jpg",
+        "image36.jpg",
+        "image37.jpg",
+        "image38.jpg",
+        "image39.jpg",
+        "image40.jpg",
+        "image41.jpg",
+        "image42.jpg",
+        "image43.jpg",
+        "image44.jpg",
+        "image45.jpg",
+        "image46.jpg",
+        "image47.jpg",
+        "image48.jpg",
+        "image49.jpg",
+        "image50.jpg",
+        "image51.jpg",
+        "image52.jpg",
+    ]
 
+
+    return render_template("gallery.html", images=images)
 
 
 if __name__ == '__main__':
